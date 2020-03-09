0bdb518c1cbecce77045e8f57f023a207cd3b76a
Adrian Niu <niuxx107@umn.edu>
2020-03-09 03:43:45 -0500

    Update the total salary and table

--- scripts/client.js
+++ scripts/client.js
@@ -4,5 +4,99 @@ function onReady() {
 
     console.log('js');
 
-   
+    $("#btn-submit").on('click', handleClick);
+
+    addSalary();
+}
+
+//global list of employees
+let employeeList = [];
+
+function handleClick(event) {
+    console.log('in handleClick');
+    event.preventDefault();
+
+    //Get our input values
+    const fname = $('#first-name').val();
+    const lname = $('#last-name').val();
+    const id = $('#id').val();
+    const title = $('#title').val();
+    const salary = $('#annual-salary').val();
+
+    console.log('Got the inputs:', fname, lname, id, title, salary);
+
+    $('#first-name').val('');
+    $('#last-name').val('');
+    $('#id').val('');
+    $('#title').val('');
+    $('#annual-salary').val('');
+
+
+    addEmployee(fname, lname, id, title, salary);
+    console.log('All employee', employeeList);
+
+    // //Show our pets on thee DOM
+    appendEmployeesToDom()
+
+    addSalary();
+}
+
+
+function addEmployee(fname, lname, id, title, salary) {
+    console.log('In addEmployee');
+    let employee = { fname, lname, id, title, salary }
+    console.log('New employee is:', employee);
+    employeeList.push(employee);
+}
+
+function appendEmployeesToDom() {
+    console.log('In-appendEmployeeToDom');
+    $('#myTable').empty();
+    for (let i = 0; i < employeeList.length; i++) {
+        let employee = employeeList[i];
+        let table = document.getElementById("myTable");
+        let row = table.insertRow(0);
+        let cell1 = row.insertCell(0);
+        let cell2 = row.insertCell(1);
+        let cell3 = row.insertCell(2);
+        let cell4 = row.insertCell(3);
+        let cell5 = row.insertCell(4);
+        let cell6 = row.insertCell(5);
+        cell1.innerHTML = employeeList[i].fname;
+        cell2.innerHTML = employeeList[i].lname;
+        cell3.innerHTML = employeeList[i].id;
+        cell4.innerHTML = employeeList[i].title;
+        cell5.innerHTML = employeeList[i].salary;
+        cell6.innerHTML = '';
+
+    } 
+    // // starting variable name with $ because it came from Jquery 
+    // //convention, not wrong if you don't do this
+    // const $ul = $('#list-pets');
+    // //Empty the ul element first
+    // $ul.empty();
+
+    // //Append all the pets to the appendPetsToDom
+    // for (const employee of employeeList) {
+    //     let $li = $(`<li>The ${employee.fname}  ${employee.lname} is with id</li>`)
+    //     // $ul.append( `<li>${pet.name} is a ${pet.type}.</li>` );
+
+    //     $ul.append($li);
+    }
+
+function addSalary() {
+    console.log('in add salary');
+    let totalSalary = 0;
+    for (let h = 0; h < employeeList.length; h++) {
+        totalSalary += Number(employeeList[h].salary);
+        console.log('total salary', employeeList[h].salary);
+        let el = $('#totalsalary');
+        el.empty();
+        el.append(totalSalary);
+    }
+    //  {
+    //         let el = $('maxcars');
+    //         el.append('No available parking spots!');
+    //         console.log('No available parking spots');
+    //     }
 }
